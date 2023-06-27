d38c64cd2e9867a61c71aa00e217bf6b41ba8c9d
chars32 <chars24@gmail.com>
2023-06-26 22:05:24 -0600

    Refactor V1.1

--- components/InventarioComponent.jsx
+++ components/InventarioComponent.jsx
@@ -9,6 +9,9 @@ function InventarioComponent() {
   const [inputSearchBar, setInputSearchBar] = useState()
   const [dataAditivos, setDataAditivos] = useState([])
 
+  // Filtrar solo los aditivos que tengan stock mayor a 0 
+  const dataAditivosFilter = dataAditivos.filter(item => item.STOCK > 0)
+
   // Funcion finge obtener los datos de una api (data.json)
   const fetchDataAditives = async () => {
     await new Promise(resolve => setTimeout(resolve, 3000))
@@ -20,8 +23,27 @@ function InventarioComponent() {
     fetchDataAditives()
   }, [])
 
-  // Filtrar solo los aditivos que tengan stock mayor a 0 
-  const dataAditivosFilter = dataAditivos.filter(item => item.STOCK > 0)
+  // useEffect para filrar los aditivos segun cambie el input 
+  // de inicio muestra la lista completa ya que al no haber datos 
+  // en el input no tiene nada que comparar.
+  useEffect(() => {
+    if (inputSearchBar) {
+      const lowerCaseSearchInput = inputSearchBar.toLowerCase()
+
+      const filteredData = dataAditivosFilter.filter(item => {
+        const lowercaseNomenclatura = item.NOMENCLATURA.toLowerCase()
+        const lowercaseDescripcion = item.DESCRIPCION.toLowerCase()
+
+        return lowercaseNomenclatura.includes(lowerCaseSearchInput) || lowercaseDescripcion.includes(lowerCaseSearchInput) && item
+      })
+      setDataAditivos(filteredData)
+    } else {
+      setDataAditivos(aditivos)
+    }
+
+  }, [inputSearchBar, dataAditivosFilter])
+
+
 
   return (
     <>
--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -9,28 +9,6 @@ function SearchBar({ setInputSearchBar }) {
     setInputSearchBar(e.target.value)
   }
 
-  // useEffect para filrar los aditivos segun cambie el input 
-  // de inicio muestra la lista completa ya que al no haber datos 
-  // en el input no tiene nada que comparar.
-  // useEffect(() => {
-  //   const filterData = () => {
-
-  //     return dataAditivosFilter.filter(item => {
-  //       const lowerCaseSearchInput = searchInput.toLowerCase()
-  //       const lowercaseNomenclatura = item.NOMENCLATURA.toLowerCase()
-  //       const lowercaseDescripcion = item.DESCRIPCION.toLowerCase()
-
-  //       return lowercaseNomenclatura.includes(lowerCaseSearchInput) || lowercaseDescripcion.includes(lowerCaseSearchInput) && item
-  //     }
-  //     )
-  //   }
-
-  //   const results = filterData();
-  //   setFilteredData(results)
-
-  // }, [dataAditivosFilter, searchInput])
-
-
   return (
     <div className="form-group">
       <div className="input-group mb-3">
