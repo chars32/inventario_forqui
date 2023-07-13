2e63861b186cdd794cd5339ac14138ff3ad8e193
Frontend <frontend@healonsite.mx>
2023-07-13 14:16:40 -0600

    PDF Remisión terminado v.1

--- app/pdfRemision/page.jsx
+++ app/pdfRemision/page.jsx
@@ -6,9 +6,8 @@ function PageRemision() {
   return (
     <PDFViewer style={{ width: '100%', height: '100vh' }}>
       <Document>
-        <Page size='A4'>
-          {/* Este es el View que será el encabezado.
-            TODO: Intentar hacer un componente ya que se usara en otros futuros pdf */}
+        <Page size='A4' style={{ fontSize: '10px' }}>
+          {/* Este  encabezado del pdf */}
           <View style={{ margin: '5% 5% 1% 5%', border: '1px solid black', display: 'flex', flexDirection: 'row', height: '9%' }}>
             <Image
               source={'https://i.ibb.co/sKVSXtV/logo-act.png'}
@@ -51,8 +50,8 @@ function PageRemision() {
               </View>
             </View>
           </View>
-          {/* Datos del cliente */}
-          <View style={{ margin: '0 5% 0 5%', border: '1px solid black', fontSize: '10px', height: '8vh' }}>
+          {/* Datos del cliente pdf */}
+          <View style={{ margin: '0 5% 0 5%', border: '1px solid black', height: '8vh' }}>
             <View style={{ display: 'flex', flexDirection: 'row', height: '100%' }}>
               <View style={{ width: '81.5%', display: 'flex', flexDirection: 'row' }}>
                 <View style={{ width: '30%', borderRight: '1px solid black' }}>
@@ -109,7 +108,7 @@ function PageRemision() {
               </View>
             </View>
           </View>
-          <View style={{ margin: '0 5% 0 5%', border: '1px solid black', borderTop: 'none', fontSize: '10px', height: '5vh' }}>
+          <View style={{ margin: '0 5% 0 5%', border: '1px solid black', borderTop: 'none', height: '5vh' }}>
             <View style={{ display: 'flex', flexDirection: 'row', height: '40%' }}>
               <View style={{ backgroundColor: 'yellow', width: '60%', borderRight: '1px solid black', borderBottom: '1px solid black', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
                 <Text>
@@ -142,6 +141,115 @@ function PageRemision() {
                 <Text>
                   01
                 </Text>
+              </View>
+            </View>
+          </View>
+          {/* Cuerpo del pdf */}
+          <View style={{ margin: '0 5% 0 5%', border: '1px solid black', marginTop: '1%', height: '50vh' }}>
+            <View style={{ display: 'flex', flexDirection: 'row', height: '5%' }}>
+              <View style={{ backgroundColor: 'yellow', width: '10%', display: 'flex', justifyContent: 'center', alignItems: 'center', borderBottom: '1px solid black', borderRight: '1px solid black' }}>
+                <Text>Partida</Text>
+              </View>
+              <View style={{ backgroundColor: 'yellow', width: '10%', display: 'flex', justifyContent: 'center', alignItems: 'center', borderBottom: '1px solid black', borderRight: '1px solid black' }}>
+                <Text>Cantidad</Text>
+              </View>
+              <View style={{ backgroundColor: 'yellow', width: '12%', display: 'flex', justifyContent: 'center', alignItems: 'center', borderBottom: '1px solid black', borderRight: '1px solid black' }}>
+                <Text>Unidad</Text>
+              </View>
+              <View style={{ backgroundColor: 'yellow', width: '68%', display: 'flex', justifyContent: 'center', alignItems: 'center', borderBottom: '1px solid black' }}>
+                <Text>Descripcion</Text>
+              </View>
+            </View>
+            <View style={{ display: 'flex', flexDirection: 'row', height: '80%' }}>
+              <View style={{ width: '10%', display: 'flex', alignItems: 'center', borderBottom: '1px solid black', borderRight: '1px solid black' }}>
+                <Text style={{ marginTop: '20%' }}>Partida</Text>
+                <Text style={{ marginTop: '20%' }}>Partida</Text>
+              </View>
+              <View style={{ width: '10%', display: 'flex', alignItems: 'center', borderBottom: '1px solid black', borderRight: '1px solid black' }}>
+                <Text style={{ marginTop: '20%' }}>Cantidad</Text>
+                <Text style={{ marginTop: '20%' }}>Cantidad</Text>
+              </View>
+              <View style={{ width: '12%', display: 'flex', alignItems: 'center', borderBottom: '1px solid black', borderRight: '1px solid black' }}>
+                <Text style={{ marginTop: '16.5%' }}>Unidad</Text>
+                <Text style={{ marginTop: '16.5%' }}>Unidad</Text>
+              </View>
+              <View style={{ width: '68%', display: 'flex', alignItems: 'center', borderBottom: '1px solid black' }}>
+                <Text style={{ marginTop: '2.8%' }}>Descripciones</Text>
+                <Text style={{ marginTop: '2.8%' }}>Descripciones</Text>
+              </View>
+            </View>
+            <View style={{ height: '15%' }}>
+              <View style={{ backgroundColor: 'yellow', width: '32%', height: '40%', display: 'flex', justifyContent: 'center', alignItems: 'center', borderBottom: '1px solid black', borderRight: '1px solid black' }}>
+                <Text>Observaciones</Text>
+              </View>
+              <View>
+                <Text>Aqui van las observaciones</Text>
+              </View>
+            </View>
+          </View>
+          {/* Footer del pdf */}
+          <View style={{ margin: '1% 5% 0 5%', border: '1px solid black', height: '17vh', display: 'flex', flexDirection: 'row' }}>
+            <View style={{ width: '50%' }}>
+              <View style={{ width: '100%', borderRight: '1px solid black', borderBottom: '1px solid black', backgroundColor: 'yellow', height: '30%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                <Text>Cliente</Text>
+                <Text>(Recibi de conformidad el material especificado)</Text>
+              </View>
+              <View style={{ width: '100%', borderRight: '1px solid black', height: '70%', display: 'flex', flexDirection: 'row' }}>
+                <View style={{ borderRight: '1px solid black', width: '35%' }}>
+                  <View style={{ display: 'flex', justifyContent: 'center', alignItems: 'flex-end', height: '25%' }}>
+                    <Text>Nombre: </Text>
+                  </View>
+                  <View style={{ display: 'flex', justifyContent: 'center', alignItems: 'flex-end', height: '25%' }}>
+                    <Text>No. Empleado: </Text>
+                  </View>
+                  <View style={{ display: 'flex', justifyContent: 'center', alignItems: 'flex-end', height: '25%' }}>
+                    <Text>Firma: </Text>
+                  </View>
+                  <View style={{ display: 'flex', justifyContent: 'center', alignItems: 'flex-end', height: '25%' }}>
+                    <Text>Cargo: </Text>
+                  </View>0000
+                </View>
+                <View style={{ width: '65%' }}>
+                  <View style={{ display: 'flex', height: '25%', borderBottom: '1px solid black' }}>
+                  </View>
+                  <View style={{ display: 'flex', height: '25%', borderBottom: '1px solid black' }}>
+                  </View>
+                  <View style={{ display: 'flex', height: '25%', borderBottom: '1px solid black' }}>
+                  </View>
+                  <View style={{ display: 'flex', height: '25%' }}>
+                  </View>
+                </View>
+              </View>
+            </View>
+            <View style={{ width: '50%' }}>
+              <View style={{ width: '100%', borderRight: '1px solid black', borderBottom: '1px solid black', backgroundColor: 'yellow', height: '30%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                <Text>FORQUIMIA</Text>
+              </View>
+              <View style={{ width: '100%', borderRight: '1px solid black', height: '70%', display: 'flex', flexDirection: 'row' }}>
+                <View style={{ borderRight: '1px solid black', width: '35%' }}>
+                  <View style={{ display: 'flex', justifyContent: 'center', alignItems: 'flex-end', height: '25%' }}>
+                    <Text>Nombre: </Text>
+                  </View>
+                  <View style={{ display: 'flex', justifyContent: 'center', alignItems: 'flex-end', height: '25%' }}>
+                    <Text>No. Empleado: </Text>
+                  </View>
+                  <View style={{ display: 'flex', justifyContent: 'center', alignItems: 'flex-end', height: '25%' }}>
+                    <Text>Firma: </Text>
+                  </View>
+                  <View style={{ display: 'flex', justifyContent: 'center', alignItems: 'flex-end', height: '25%' }}>
+                    <Text>Cargo: </Text>
+                  </View>0000
+                </View>
+                <View style={{ width: '65%' }}>
+                  <View style={{ display: 'flex', height: '25%', borderBottom: '1px solid black' }}>
+                  </View>
+                  <View style={{ display: 'flex', height: '25%', borderBottom: '1px solid black' }}>
+                  </View>
+                  <View style={{ display: 'flex', height: '25%', borderBottom: '1px solid black' }}>
+                  </View>
+                  <View style={{ display: 'flex', height: '25%' }}>
+                  </View>
+                </View>
               </View>
             </View>
           </View>
